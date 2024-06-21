603a5602d18b55e3c304e1a0eeb86ed4f16cb086
Jakemathewsunil <jakesinisunil@gmail.com>
2024-06-21 18:30:23 +0530

    bug fixed- unwanted sites being blocked

--- content.js
+++ content.js
@@ -1,16 +1,21 @@
-document.documentElement.innerHTML = `
-    <style>
-        body {
-            background-color: black;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            color: white;
-            font-size: 24px;
-            font-family: Arial, sans-serif;
-        }
-    </style>
-    <div>This site is blocked</div>
+document.body.innerHTML = `
+    <div id="blocker-message" style="
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 9999;
+        color: white;
+        font-size: 24px;
+        font-family: Arial, sans-serif;
+    ">
+        This site is blocked
+    </div>
 `;
+document.body.style.overflow = 'hidden';
+document.body.style.pointerEvents = 'none';
